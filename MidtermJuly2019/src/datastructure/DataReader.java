1959d215cf4118111f2fb8d8692260b6b5c9b941
cidwells <cidwells@gmail.com>
2020-01-12 12:35:53 -0500

    file reader completed

--- MidtermJuly2019/src/datastructure/DataReader.java
+++ MidtermJuly2019/src/datastructure/DataReader.java
@@ -1,8 +1,16 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class DataReader {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * User API to read the below textFile and print to console.
 		 * Use BufferedReader class. 
@@ -18,8 +26,49 @@ public class DataReader {
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String textFile = System.getProperty("user.dir")+"/src/data/self-driving-car";
+		FileReader fr = null;
+		BufferedReader br = null;
+		String line;
+		String store = "";
 
+		try{
+			fr = new FileReader(textFile);
+
+
+		}catch(Exception e){
+			System.out.println("Could not find file ");
+		}
+
+		try{
+			br = new BufferedReader(fr);
+			while((line =br.readLine())!= null){
+				System.out.println(line);
+				store+= line;
+
+			}
+		}catch(Exception e){
+			System.out.println("Cannot read file ");
+		}
+
+		String[] storeArray = store.split(" ");
+
+		Stack<String> myStack = new Stack<String>();
+		List<String> myList = new LinkedList<String>();
+		for(String element : storeArray){
+			myStack.add(element);
+			myStack.push(element);
+		}
+		System.out.println("The LinkedList LIFO");
+		Iterator<String> it = myList.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next() + " ");
+		}
+		System.out.println(" The Stack  LIFO");
+
+		while(!myStack.isEmpty()){
+			System.out.println(myStack.pop() + "  ");
+		}
 
 
 	}
