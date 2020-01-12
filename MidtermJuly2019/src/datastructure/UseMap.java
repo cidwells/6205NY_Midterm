2a2d3f50faa35f27cd331010aeb8044b76319192
cidwells <cidwells@gmail.com>
2020-01-12 16:26:59 -0500

    use map done

--- MidtermJuly2019/src/datastructure/UseMap.java
+++ MidtermJuly2019/src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +12,39 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+		ArrayList<String>mainCourse = new ArrayList<String>();
+		mainCourse.add("Steak");
+		mainCourse.add("Salmon");
+		mainCourse.add("Drunken Noodle");
+		mainCourse.add("Pho");
+
+		ArrayList<String>appetizer= new ArrayList<String>();
+		appetizer.add("Spring rolls");
+		appetizer.add("Shrimp dumplings");
+		appetizer.add("Sweet potato fries");
+		appetizer.add("Samosa");
+
+		ArrayList<String>drink = new ArrayList<String>();
+		drink.add("Coke");
+		drink.add("Pina Coloda");
+		drink.add("Borhani");
+		drink.add("Lache");
+
+		HashMap<String, List<String>> mapList = new HashMap<String, List<String>>();
+
+		mapList.put("Main Course", mainCourse);
+		mapList.put("Appetizer", appetizer);
+		mapList.put("Drink", drink);
+
+		for (Map.Entry<String,List<String>> hm : mapList.entrySet()){
+			System.out.println( hm.getKey()+ "--------------"+hm.getValue());
+		}
+		System.out.println(" ");
+		Set set = mapList.entrySet();
+		Iterator it = set.iterator();
+		while (it.hasNext()){
+			System.out.println(it.next());
+		}
 
 	}
 
