eb1004f21f58b12b4a427aceaef5795681ed9f4f
cidwells <cidwells@gmail.com>
2020-01-12 15:19:25 -0500

    Array list done

--- MidtermJuly2019/src/datastructure/UseArrayList.java
+++ MidtermJuly2019/src/datastructure/UseArrayList.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import javafx.css.StyleableStringProperty;
+
+import javax.print.DocFlavor;
 import java.util.ArrayList;
 
 public class UseArrayList {
@@ -11,8 +14,22 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
+       ArrayList<String> myLocation = new ArrayList<String>();
+       	myLocation.add("Brooklyn");
+		myLocation.add("Queens");
+		myLocation.add("Manhattan");
+		myLocation.add("Staten Island");
+		myLocation.add("Bronx");
+		myLocation.add("Newark");
+		for(String location : myLocation){
+			System.out.println(location);
+		}
+		System.out.println("Last index value is:"+myLocation.get(myLocation.size()-1));
 
-
+		while (!myLocation.isEmpty()){
+			System.out.println(myLocation);
+			myLocation.remove(myLocation.size()-1);
+		}
 	}
 
 }
