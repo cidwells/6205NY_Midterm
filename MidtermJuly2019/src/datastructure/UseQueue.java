0d608f5d000b9c49d008f429c3c77770db1b8ddb
cidwells <cidwells@gmail.com>
2020-01-16 15:18:13 -0500

    Queue has been completed

--- MidtermJuly2019/src/datastructure/UseQueue.java
+++ MidtermJuly2019/src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,33 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<String> myQueue = new LinkedList<>();
+		myQueue.add("Sun");
+		myQueue.add("Mercury");
+		myQueue.add("Venus");
+		myQueue.add("Earth");
+		myQueue.add("Mars");
+		myQueue.add("Jupiter");
+		myQueue.add("Saturn");
+		myQueue.add("Neptune");
+		myQueue.add("Uranus");
+		myQueue.add("Pluto");
+		if(myQueue!= null){
+			System.out.println("\nThe first celestial body is the " + myQueue.peek());
+
+		}
+		System.out.println("\nRemoving the first celestial object");
+		System.out.println("The "+ myQueue.poll() + " has been removed \n");
+		Iterator myIterator = myQueue.iterator();
+		while (myIterator.hasNext()){
+			System.out.println(myIterator.next());
+		}
+		System.out.println("\nRemoving any non-planetary object ");
+		myQueue.remove("Pluto");
+		System.out.println("\nPlanets of the solar system");
+		for (String planets : myQueue){
+			System.out.println(planets);
+		}
 
 	}
 
